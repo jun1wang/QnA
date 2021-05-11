77a8bf1ffa860e040011576764a98f2bc9429df0
Jun Wang <jun1wang@gmail.com>
2021-05-10 18:23:38 -0700

    added backend

---
+++ backend/QnA/Data/IDataRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using QnA.Data.Models;
+
+namespace QnA.Data
+{
+    public interface IDataRepository
+    {
+        IEnumerable<QuestionGetManyResponse> GetQuestions();
+
+        IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search);
+
+        IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
+
+        QuestionGetSingleResponse GetQuestion(int questionId);
+
+        bool QuestionExists(int questionId);
+
+        AnswerGetResponse GetAnswer(int answerId);
+
+        QuestionGetSingleResponse PostQuestion(QuestionPostRequest question);
+
+        QuestionGetSingleResponse PutQuestion(int questionId, QuestionPutRequest question);
+
+        void DeleteQuestion(int questionId);
+
+        AnswerGetResponse PostAnswer(AnswerPostRequest answer);
+
+    }
+}
